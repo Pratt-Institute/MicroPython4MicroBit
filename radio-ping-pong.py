b3a27bb1a7a2ad6429fbed5aaa48c1f6b7bb20fd
Pratt School of Information | Raspberry Pi Projects <mmaceli@pratt.edu>
2020-02-24 09:46:41 -0500

    Add files via upload

--- radio-ping-pong.py
+++ radio-ping-pong.py
@@ -3,6 +3,7 @@ from microbit import *
 
 # The radio won't work unless it's switched on.
 radio.on()
+radio.config(channel=7)
 
 # keep score
 messages_rec = 0
@@ -12,13 +13,11 @@ while True:
     # Button A sends ping
     if button_a.was_pressed():
         radio.send('ping')
-    # Button A sends ping
+    # Button B prints total so far 
     if button_b.was_pressed():
         display.scroll(messages_rec)
     # Read any incoming messages.
     incoming = radio.receive()
     if incoming == 'ping':
-        pin0.write_digital(1)  # turn pin0 (and the LED) on
-        sleep(500)             # delay for half a second (500 milliseconds)
-        pin0.write_digital(0)  # turn pin0 (and the LED) off
+        display.show(Image.SQUARE_SMALL, delay=100, wait=False, clear=True)
         messages_rec += 1
